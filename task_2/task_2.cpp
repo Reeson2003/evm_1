e72409c215143fd8e67091562beb84285df5cec5
reeson2003 <reeson2003@gmail.com>
2018-10-28 14:09:23 +0300

    2 task ready

--- task_2/task_2.cpp
+++ task_2/task_2.cpp
@@ -1,50 +1,42 @@
-#include <stdlib.h>
-#include <string.h>
 #include <ncurses.h>
 #include <unistd.h>
 
-using namespace std;
+static const int START_X = 25;
+static const int START_Y = 5;
+static const int END_X = 55;
+static const int END_Y = 15;
+static const int DELAY = 600000;
+static const int SCROLL = 1;
 
-void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string);
+static const int HEIGHT = END_Y - START_Y;
+static const int WIDTH = END_X - START_X;
+static const char *COLOR_NAMES[] = {
+        "BLACK",
+        "RED",
+        "GREEN",
+        "YELLOW",
+        "BLUE",
+        "MAGENTA",
+        "CYAN",
+        "WHITE"
+};
 
 int main(int argc, char *argv[]) {
-    initscr();            /* Start curses mode 		*/
-    if (has_colors() == FALSE) {
-        endwin();
-        printf("Your terminal does not support color\n");
-        exit(1);
-    }
-    start_color();            /* Start color 			*/
-    for (int i = 0; i < COLORS; ++i) {
-        for (int j = 0; j < COLORS; ++j) {
+    initscr();
+    WINDOW *window = newwin(HEIGHT, WIDTH, START_Y, START_X);
+    start_color();
+    scrollok(window, true);
+    for (short i = 0; i < 8; ++i) {
+        for (short j = 0; j < 8; ++j) {
             init_pair(1, i, j);
-            attron(COLOR_PAIR(1));
-            print_in_middle(stdscr, 5, 25, 30, "Viola !!! In color ...");
-            attroff(COLOR_PAIR(1));
-            usleep(600000);
+            wattron(window, COLOR_PAIR(1));
+            wprintw(window, "Фон %s; Сим %s\n", COLOR_NAMES[j], COLOR_NAMES[i]);
+            wrefresh(window);
+            wscrl(window, 0);
+            wattroff(window, COLOR_PAIR(1));
+            usleep(DELAY);
         }
     }
-    getch();
+    wgetch(window);
     endwin();
-}
-
-void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string) {
-    int length, x, y;
-    float temp;
-
-    if (win == NULL)
-        win = stdscr;
-    getyx(win, y, x);
-    if (startx != 0)
-        x = startx;
-    if (starty != 0)
-        y = starty;
-    if (width == 0)
-        width = 80;
-
-    length = strlen(string);
-    temp = (width - length) / 2;
-    x = startx + (int) temp;
-    mvwprintw(win, y, x, "%s", string);
-    refresh();
 }
